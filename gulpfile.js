14d6ad06ae2a9e7446b0e1928fbe976bbe044259
下坂　辰弥 <>
2018-05-26 21:25:03 +0900

    view用の仕組みを設定。

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var autoprefixer =require('gulp-autoprefixer');
 var ejs = require("gulp-ejs");
-
 var merge = require('merge-stream');
 
 var jsonData = require('./_dev_files/json/index.json');
@@ -48,18 +47,23 @@ gulp.task('build-stg', function(){
 	return merge(buildStgPc, buildStgSp);
 });
 
-// ローカルサーバーに関するタスク
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: "./"
-        }
-    });
-});
-
-// 自動でローカルサーバーをリロードするタスク
-gulp.task('bs-reload', function () {
-    browserSync.reload();
+// viewに関するタスク
+gulp.task('build-view', function(){
+	var buildViewPc = gulp.src('./base_files/template_ejs/template_view_pc.ejs')
+	.pipe(ejs({
+		jsonData: jsonData //jsonData に data.json を取り込む
+	}))
+	.pipe(rename("pc.html"))
+	.pipe(gulp.dest('./view_files'));
+	
+	var buildViewSp = gulp.src('./base_files/template_ejs/template_view_sp.ejs')
+	.pipe(ejs({
+		jsonData: jsonData //jsonData に data.json を取り込む
+	}))
+	.pipe(rename("sp.html"))
+	.pipe(gulp.dest('./view_files'));
+	
+	return merge(buildViewPc, buildViewSp);
 });
 
 // cssに関するタスク
@@ -74,10 +78,24 @@ gulp.task('build-css', function () {
 	.pipe(gulp.dest('./base_files/css'));
 });
 
+// ローカルサーバーに関するタスク
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: "./view_files"
+        }
+    });
+});
+
+// 自動でローカルサーバーをリロードするタスク
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
 // ファイルの変更を監視
 gulp.task('watch', function() {
-	gulp.watch(['./_dev_files/scss/*.scss','./base_files/css/*.css'], ['build-css','build-mock','build-stg','bs-reload'])
-	gulp.watch(['./_dev_files/parts_ejs/*.ejs'], ['build-mock','build-stg','bs-reload'])
+	gulp.watch(['./_dev_files/scss/*.scss','./base_files/css/*.css'], ['build-css','build-mock','build-stg','build-view','bs-reload'])
+	gulp.watch(['./_dev_files/parts_ejs/*.ejs'], ['build-mock','build-stg','build-view','bs-reload'])
 });
 
 // デフォルトタスク
